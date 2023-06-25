e6d50cbb73d8b5e29a4a50ea933ce691006b7b33
aleenaa367 <136946063+aleenaa367@users.noreply.github.com>
2023-06-25 04:30:43 +0000

    assignment2-2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
+    
     celsius = input('input celsius: ')
     fahrenheit = (int(celsius) * 9/5) + 32
     print (f'fahrenheit \t {fahrenheit:.2f}')
